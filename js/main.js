9999ffacec034981e5e7d9d22f757d36ff56e6ec
Vinícius Silva <vinisilva45@live.com>
2020-10-02 15:05:31 -0300

    Alterando o main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-alert("meu primeiro alert");
+var nome = "Vinicius Rosa";
+var n1 = 5;
+var n2 = 20;
+var frase = "Japão é o melhor time";
+
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
