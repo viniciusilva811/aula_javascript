3332bdb8ec9735508df6f6f89c848f5f1c48606b
Vinicius <vinisilva45@live.com>
2020-10-05 16:04:00 -0300

    Usando funções no JavaScript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 
+function soma(n1,n2){
+    return n1+n2;
+}
+
+function validaIdade(idade){
+    var validar;
+    if(idade >= 18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+}
+
+var idade = prompt("Qual é a sua idade ?");
+console.log(validaIdade(idade));
+alert("Sua idade é " + idade + " anos");
+
+
+
+/*
 var frutas = [{nome: "fruta", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
 console.log(frutas);
 alert(frutas[1].nome);
 
-
-/*
 var nome = "Vinicius Rosa";
 var n1 = 5;
 var n2 = 20;
