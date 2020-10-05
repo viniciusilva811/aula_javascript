9834590e9d879a08b24c63d92857daf0fc05a233
Vinicius <vinisilva45@live.com>
2020-10-05 15:07:37 -0300

    Uso de Array e Dicionario

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+
+var frutas = [{nome: "fruta", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+/*
 var nome = "Vinicius Rosa";
 var n1 = 5;
 var n2 = 20;
@@ -6,3 +13,4 @@ var frase = "Japão é o melhor time";
 console.log(nome);
 console.log(n1 * n2);
 console.log(frase.toLowerCase());
+*/
